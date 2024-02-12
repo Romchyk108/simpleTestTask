4f27164d3a253a2fcdcb5e947de880e7dd20819e
roman_shestopal@gmail.com <romchyk.r@gmail.com>
2024-02-12 16:07:16 +0200

    Initial Commit

---
+++ SandstormSoftwareTestTask/SandstormSoftwareTestTaskApp.swift
@@ -0,0 +1,32 @@
+//
+//  SandstormSoftwareTestTaskApp.swift
+//  SandstormSoftwareTestTask
+//
+//  Created by Roman Shestopal on 12.02.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SandstormSoftwareTestTaskApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
